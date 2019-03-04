70c1c8676bb74f9ceebfbdb7cf6c9a8f7747a8ec
Mityai Kowalski <kovvalsking@gmail.com>
2019-03-04 22:07:39 +0200

    add countdown

---
+++ src/components/featured/TimeUntil.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
+
+class TimeUntil extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+
+            <Slide left delay={1000}>
+                <div className="countdown_wrapper">
+                    <div className="countdown_top">
+                        Event starts in
+                    </div>
+                    <div className="countdown_bottom">
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                23
+                            </div>
+                            <div className="countdown_tag">
+                                Days
+                            </div>
+                        </div>
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                41
+                            </div>
+                            <div className="countdown_tag">
+                                Hs
+                            </div>
+                        </div>
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                21
+                            </div>
+                            <div className="countdown_tag">
+                                Min
+                            </div>
+                        </div>
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                56
+                            </div>
+                            <div className="countdown_tag">
+                                Sec
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
+        );
+    }
+}
+
+export default TimeUntil;
--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carrousel from './Carrousel';
+import TimeUntil from './TimeUntil';
 
 const Featured = () => {
     return (
@@ -13,6 +14,8 @@ const Featured = () => {
                     Ariana Grande
                 </div>
             </div>
+
+            <TimeUntil />
 
         </div>
     );
